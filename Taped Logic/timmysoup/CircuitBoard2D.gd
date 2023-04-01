f3ec8a768427785bd40ba0783efe69350987e1bc
GodMadeEarth <88457199+GodMadeEarth@users.noreply.github.com>
2023-04-01 18:38:24 -0400

    Circuit Board v4

--- Taped Logic/timmysoup/CircuitBoard2D.gd
+++ Taped Logic/timmysoup/CircuitBoard2D.gd
@@ -1,11 +1,21 @@
 extends TileMap
 
+#right = 1>9
+#bottomRight = 3>7-15
+#bottom = 5>13
+#bottomLeft = 7>3-11
+#left = 9>1
+#topLeft = 11>7-15
+#top = 13>5
+#topRight = 15>3-11
 
 
-# Called when the node enters the scene tree for the first time.
+
+
+
 func _ready():
-	print(tiles_connect())
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	print(tiles_connected())
+
 func _process(delta):
 	pass
 
@@ -20,5 +30,72 @@ func get_components_of_type(atlasCord = Vector2i(0,1)):
 func emit_power(cellPosition = Vector2i(0,1)):
 	get_cell_tile_data(1,cellPosition).set_custom_data("powerPathing",1)
 	
-func  tiles_connect(cell1Position = Vector2i(1,1),cell2Position = Vector2i(0,0)):
-	return get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(11)
+func tiles_connected(cell1Position = Vector2i(1,1),cell2Position = Vector2i(0,1)):
+	var direction = Vector2i(cell1Position - cell2Position)
+	
+	if direction == Vector2i(1,0):#Left
+		if get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(7) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(3) == 0:
+			return true
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(9) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(1) == 0:
+			return true
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(11) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(15) == 0:
+			return true
+
+	elif direction == Vector2i(-1,0):#Right
+		if get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(3) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(7) == 0:
+			return true
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(1) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(9) == 0:
+			return true
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(15) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(11) == 0:
+			return true
+			
+	elif  direction == Vector2i(0,1):#Up
+		if get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(11) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(7) == 0:
+			return true
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(13) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(5) == 0:
+			return true
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(15) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(3) == 0:
+			return true
+			
+	elif direction == Vector2i(0,-1):#Down
+		if get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(7) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(11) == 0:
+			return true
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(5) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(13) == 0:
+			return true
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(3) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(15) == 0:
+			return true
+	
+	else:
+		return false
+
+func get_wire_group(rootCellPosition = Vector2i(0,1)):
+	var processedWires = []
+	var wiresToProcess = [rootCellPosition]
+	var quantumWire = Vector2i()
+	while wiresToProcess.size() != 0:
+		quantumWire = wiresToProcess[0]
+		quantumWire.x += 1
+		if quantumWire in get_used_cells(1):
+			if tiles_connected(wiresToProcess[0],quantumWire):
+				wiresToProcess.append(quantumWire)
+				
+		quantumWire = wiresToProcess[0]
+		quantumWire.x -= 1
+		if quantumWire in get_used_cells(1):
+			if tiles_connected(wiresToProcess[0],quantumWire):
+				wiresToProcess.append(quantumWire)
+				
+		quantumWire = wiresToProcess[0]
+		quantumWire.y += 1
+		if quantumWire in get_used_cells(1):
+			if tiles_connected(wiresToProcess[0],quantumWire):
+				wiresToProcess.append(quantumWire)
+				
+		quantumWire = wiresToProcess[0]
+		quantumWire.y -= 1
+		if quantumWire in get_used_cells(1):
+			if tiles_connected(wiresToProcess[0],quantumWire):
+				wiresToProcess.append(quantumWire)
+		
+		processedWires.append(wiresToProcess[0]);wiresToProcess.erase(0)
+	return processedWires
