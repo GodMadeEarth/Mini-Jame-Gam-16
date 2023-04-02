14462bbe68632c510126015a8177a24e3f627510
GodMadeEarth <88457199+GodMadeEarth@users.noreply.github.com>
2023-04-01 20:38:54 -0400

    Circuit Board v6

--- Taped Logic/timmysoup/CircuitBoard2D.gd
+++ Taped Logic/timmysoup/CircuitBoard2D.gd
@@ -1,23 +1,12 @@
 extends TileMap
 
-#right = 1>9
-#bottomRight = 3>7-15
-#bottom = 5>13
-#bottomLeft = 7>3-11
-#left = 9>1
-#topLeft = 11>7-15
-#top = 13>5
-#topRight = 15>3-11
-
-
-
 
 
 func _ready():
-	print(tiles_connected())
+	print(get_wire_group(Vector2i(1,1)))
 
 func _process(delta):
-	set_cell(3,Vector2i(0,0),-1,Vector2i(0,0))
+	pass
 
 func get_components_of_type(atlasCord = Vector2i(0,1)):
 	var components = get_used_cells(0)
@@ -35,35 +24,35 @@ func tiles_connected(cell1Position = Vector2i(1,1),cell2Position = Vector2i(0,1)
 	var direction = Vector2i(cell1Position - cell2Position)
 	
 	if direction == Vector2i(1,0):#Left
-		if get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(7) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(3) == 0:
+		if get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(7) != -1 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(3) != -1:
 			return true
-		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(9) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(1) == 0:
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(8) != -1 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(0) != -1:
 			return true
-		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(11) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(15) == 0:
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(11) != -1 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(15) != -1:
 			return true
 
 	elif direction == Vector2i(-1,0):#Right
-		if get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(3) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(7) == 0:
+		if get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(3) != -1 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(7) != -1:
 			return true
-		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(1) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(9) == 0:
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(0) != -1 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(8) != -1:
 			return true
-		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(15) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(11) == 0:
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(15) != -1 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(11) != -1:
 			return true
 			
 	elif  direction == Vector2i(0,1):#Up
-		if get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(11) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(7) == 0:
+		if get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(11) != -1 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(7) != -1:
 			return true
-		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(13) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(5) == 0:
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(12) != -1 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(4) != -1:
 			return true
-		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(15) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(3) == 0:
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(15) != -1 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(3) != -1:
 			return true
 			
 	elif direction == Vector2i(0,-1):#Down
-		if get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(7) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(11) == 0:
+		if get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(7) != -1 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(11) != -1:
 			return true
-		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(5) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(13) == 0:
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(4) != -1 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(12) != -1:
 			return true
-		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(3) == 0 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(15) == 0:
+		elif get_cell_tile_data(1,cell1Position).get_terrain_peering_bit(3) != -1 and get_cell_tile_data(1,cell2Position).get_terrain_peering_bit(15) != -1:
 			return true
 	
 	else:
@@ -73,31 +62,33 @@ func get_wire_group(rootCellPosition = Vector2i(0,1)):
 	var processedWires = []
 	var wiresToProcess = [rootCellPosition]
 	var quantumWire = Vector2i()
+	
 	while wiresToProcess.size() != 0:
+		
 		quantumWire = wiresToProcess[0]
 		quantumWire.x += 1
-		if quantumWire in get_used_cells(1):
+		if quantumWire in get_used_cells(1) and quantumWire not in processedWires:
 			if tiles_connected(wiresToProcess[0],quantumWire):
 				wiresToProcess.append(quantumWire)
 				
 		quantumWire = wiresToProcess[0]
 		quantumWire.x -= 1
-		if quantumWire in get_used_cells(1):
+		if quantumWire in get_used_cells(1) and quantumWire not in processedWires:
 			if tiles_connected(wiresToProcess[0],quantumWire):
 				wiresToProcess.append(quantumWire)
 				
 		quantumWire = wiresToProcess[0]
 		quantumWire.y += 1
-		if quantumWire in get_used_cells(1):
+		if quantumWire in get_used_cells(1) and quantumWire not in processedWires:
 			if tiles_connected(wiresToProcess[0],quantumWire):
 				wiresToProcess.append(quantumWire)
 				
 		quantumWire = wiresToProcess[0]
 		quantumWire.y -= 1
-		if quantumWire in get_used_cells(1):
+		if quantumWire in get_used_cells(1) and quantumWire not in processedWires:
 			if tiles_connected(wiresToProcess[0],quantumWire):
 				wiresToProcess.append(quantumWire)
-		
-		processedWires.append(wiresToProcess[0]);wiresToProcess.erase(0)
+				
+		processedWires.append(wiresToProcess[0]);wiresToProcess.erase(wiresToProcess[0])
 	return processedWires
 
