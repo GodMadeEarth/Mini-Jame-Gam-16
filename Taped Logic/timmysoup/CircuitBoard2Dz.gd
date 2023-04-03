5e45bf5bd95d3fa4400cd91967fa1562c03fa2c1
GodMadeEarth <88457199+GodMadeEarth@users.noreply.github.com>
2023-04-02 20:17:36 -0400

    Circuit Board v11
    Finally Works!

--- Taped Logic/timmysoup/CircuitBoard2Dz.gd
+++ Taped Logic/timmysoup/CircuitBoard2Dz.gd
@@ -1,12 +1,13 @@
 extends TileMap
 
 # Load Component Atlas
-var outputComponentAtlas = [Vector2i(0,1),Vector2i(1,1),Vector2i(4,6),Vector2i(4,7),Vector2i(0,8),Vector2i(1,8),Vector2i(2,8),Vector2i(2,9),Vector2i(4,8),Vector2i(5,8)]
+var outputComponentAtlas = [Vector2i(0,1),Vector2i(1,1),Vector2i(2,8),Vector2i(2,9),Vector2i(5,8),Vector2i(4,8),Vector2i(4,7),Vector2i(4,6),Vector2i(0,8),Vector2i(1,8)]
 var inputComponentAtlas = [Vector2i(0,0),Vector2i(1,0)]
-
-var notGateAtlas = [Vector2i(2,0),Vector2i(3,0),Vector2i(4,0),Vector2i(4,1)]
-var andGateAtlas = [Vector2i(0,2),Vector2i(2,2),Vector2i(4,2),Vector2i(0,4)]
-var orGateAtlas = [Vector2i(2,4),Vector2i(4,4),Vector2i(0,6),Vector2i(1,6)]
+#[off,on]
+#[right,down,left,top]
+var notGateAtlas = [Vector2i(4,0),Vector2i(3,0),Vector2i(4,1),Vector2i(2,0)]
+var andGateAtlas = [Vector2i(2,2),Vector2i(4,2),Vector2i(0,4),Vector2i(0,2)]
+var orGateAtlas = [Vector2i(4,4),Vector2i(2,6),Vector2i(0,6),Vector2i(2,4)]
 var logicComponentAtlas = notGateAtlas + andGateAtlas + orGateAtlas
 
 var outputComponents = []
@@ -80,8 +81,11 @@ func tiles_connected(cell1Position = Vector2i(1,1),cell2Position = Vector2i(0,1)
 
 func get_wire_group(rootCellPosition = Vector2i(0,1)): #creates an array of all the positions for start?
 	var processedWires = []
-	var wiresToProcess = [rootCellPosition]
+	var wiresToProcess = []
 	var quantumWire = Vector2i()
+	
+	if rootCellPosition in get_used_cells(1):
+		wiresToProcess.append(rootCellPosition)
 	
 	while wiresToProcess.size() != 0:
 		
@@ -114,7 +118,7 @@ func get_wire_group(rootCellPosition = Vector2i(0,1)): #creates an array of all 
 
 func total_peering_bits( cellPosition = Vector2i(0,1),layer = 1):
 	var peering_bits = 0
-	for i in range(1,15):
+	for i in [0,3,4,7,8,11,12,15]:
 		if get_cell_tile_data(layer,cellPosition).get_terrain_peering_bit(i) != -1:
 			peering_bits += 1
 	return peering_bits
@@ -122,8 +126,12 @@ func total_peering_bits( cellPosition = Vector2i(0,1),layer = 1):
 func _on_button_pressed():
 	
 	for outputComponent in outputComponents:
-		update_output_component(outputComponent)
-					
+		if get_cell_atlas_coords(2,outputComponent) in outputComponentAtlas.slice(0,2):
+			if wire_powered(get_wire_group(outputComponent)):
+				update_cell_atlas(outputComponent,outputComponentAtlas[1])
+			else:
+				update_cell_atlas(outputComponent,outputComponentAtlas[0])
+
 
 func update_cell_atlas(cellPosition = Vector2i(0,0),atlasCord = Vector2i(0,0)):
 	if get_cell_atlas_coords(2,cellPosition) != atlasCord:
@@ -136,13 +144,56 @@ func get_wire_endpoints(wireArray=[]):
 			endpointArray.append(wire)
 	return endpointArray
 
-func update_output_component(outputComponent = Vector2i(0,0)):
-	var wirePoints = get_wire_endpoints(get_wire_group(outputComponent))
-	wirePoints.erase(outputComponent)
+func wire_powered(wireArray=[]):
+	
+	var wirePoints = get_wire_endpoints(wireArray)
+	var returnValue = false
+	
 	for point in wirePoints:
 		if point in inputComponents:
 			if get_cell_atlas_coords(2,point) == inputComponentAtlas[1]:
-				update_cell_atlas(outputComponent,outputComponentAtlas[1])
-				return
-			elif get_cell_atlas_coords(2,point) == inputComponentAtlas[0]:
-				update_cell_atlas(outputComponent,outputComponentAtlas[0])
+				return true
+
+		if point in logicComponents:
+			
+			var direction
+			var wire1Result
+			var wire2Result
+			
+			# Finding Direction
+			if get_cell_atlas_coords(2,point) in notGateAtlas:
+				direction = notGateAtlas.find(get_cell_atlas_coords(2,point))
+			if get_cell_atlas_coords(2,point) in andGateAtlas:
+				direction = andGateAtlas.find(get_cell_atlas_coords(2,point))
+			if get_cell_atlas_coords(2,point) in orGateAtlas:
+				direction = orGateAtlas.find(get_cell_atlas_coords(2,point))
+			
+			
+			
+			# Checking if wires conected
+			if direction == 0:
+				wire1Result = wire_powered(get_wire_group(Vector2i(point.x-1,point.y)))
+				wire2Result = wire_powered(get_wire_group(Vector2i(point.x-1,point.y-1)))
+			elif direction == 1:
+				wire1Result = wire_powered(get_wire_group(Vector2i(point.x,point.y-1)))
+				wire2Result = wire_powered(get_wire_group(Vector2i(point.x+1,point.y-1)))
+			elif direction == 2:
+				wire1Result = wire_powered(get_wire_group(Vector2i(point.x+1,point.y)))
+				wire2Result = wire_powered(get_wire_group(Vector2i(point.x+1,point.y+1)))
+			elif direction == 3:
+				wire1Result = wire_powered(get_wire_group(Vector2i(point.x,point.y+1)))
+				wire2Result = wire_powered(get_wire_group(Vector2i(point.x-1,point.y+1)))
+			
+			#print(point,direction,wire1Result,wire2Result)
+			
+			# Finding Direction
+			if get_cell_atlas_coords(2,point) in notGateAtlas:
+				if wire1Result == false:
+					return true
+			elif get_cell_atlas_coords(2,point) in andGateAtlas:
+				if wire1Result == true and wire2Result == true:
+					return true
+			elif get_cell_atlas_coords(2,point) in orGateAtlas:
+				if wire1Result == true or wire2Result == true:
+					return true
+	return returnValue
